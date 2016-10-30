6fc0724c69c97a0234c9216a06bcb2b6588642c5
PlusOne <wabisu@gmail.com>
2016-10-30 06:50:15 -0700

    40%

--- Assets/Scripts/SceneBehaviour.cs
+++ Assets/Scripts/SceneBehaviour.cs
@@ -315,7 +315,7 @@ public class SceneBehaviour : MonoBehaviour {
 
 	private int GetEyeContact ()
 	{
-		return (int)(investorFaceLookTime / totalTimeTalking * 100);
+		return (int)(investorFaceLookTime / totalTimeTalking * 100 + 5);
 	}
 
 	private int GetMemorization ()
